597588eb018ca657dd9ddf5348a9737430d95ce0
Igor Katsuba <igor@katsuba.dev>
2024-03-13 23:06:09 +0200

    fix

--- db/wrapper.sh
+++ db/wrapper.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-chown -R postgres "$PGDATA"
+# unset PGHOST to force psql to use Unix socket path
+# this is specific to Railway and allows
+# us to use PGHOST after the init
+unset PGHOST
+
+## unset PGPORT also specific to Railway
+## since postgres checks for validity of
+## the value in PGPORT we unset it in case
+## it ends up being empty
+unset PGPORT
 
 # Call the entrypoint script with the
 # approriate PGHOST & PGPORT
