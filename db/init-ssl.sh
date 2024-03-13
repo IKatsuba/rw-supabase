2bb63dd7f3f751bcdedc6420a34411130728231a
Igor Katsuba <igor@katsuba.dev>
2024-03-13 23:15:10 +0200

    fix

--- db/init-ssl.sh
+++ db/init-ssl.sh
@@ -5,6 +5,9 @@ SSL_DIR="/var/lib/postgresql/data/certs"
 # Use sudo to create the directory as root
 sudo mkdir -p "$SSL_DIR"
 
+# recursively change ownership of all files in the directory
+sudo chown postgres:postgres -R "$PGDATA"
+
 # Use sudo to change ownership as root
 sudo chown postgres:postgres "$SSL_DIR"
 
